c3dd991f0d4c6df5b702fe2354727f895ca30682
Widianto Nur Firmansah <xnaltasee@gmail.com>
2022-03-18 23:12:12 +0700

    remove these files

--- chunk.h
+++
@@ -1,4 +0,0 @@
-#ifndef chunk_h
-#define chunk_h
-
-#endif // chunk_h
--- utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef utils_h
-#define utils_h
-
-uint32_t pd_crc32(uint32_t, const void *, size_t);
-uint8_t fread_u8(FILE *);
-uint16_t fread_u16(FILE *);
-uint32_t fread_u32(FILE *);
-
-#endif // utils_h
